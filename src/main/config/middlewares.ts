22595b10820be771f508e004dd14825e7d2c4a71
Ronny Cruz <ronnyfvcruz@gmail.com>
2020-04-07 12:31:34 -0300

    feat: ensure requests cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/bodyParser';
+import { useCors } from '../middlewares/cors';
 
 const middleware = (app: Express): void => {
   app.use(bodyParser);
+  app.use(useCors);
 };
 
 export { middleware };
